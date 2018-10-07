23afe6c573e00c292db1cd3ead87ad6518e1cc55
Raman Yasel <strogofcdm@gmail.com>
2018-10-07 12:45:19 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,26 +1,38 @@
 class SmartCalculator {
   constructor(initialValue) {
     // your implementation
+    this.initialValue = initialValue;
+    this.toString = () => eval(this.initialValue);
   }
 
   add(number) {
     // your implementation
+    this.initialValue = this.initialValue + '+' + number;
+    return this;
   }
   
   subtract(number) {
     // your implementation
+    this.initialValue = this.initialValue + '-' + number;
+    return this;
   }
 
   multiply(number) {
     // your implementation
+    this.initialValue = this.initialValue + '*' + number;
+    return this;
   }
 
   devide(number) {
     // your implementation
+    this.initialValue = this.initialValue + '/' + number;
+    return this;
   }
 
   pow(number) {
     // your implementation
+    this.initialValue = this.initialValue + '**' + number;
+    return this;
   }
 }
 
